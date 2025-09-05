003e76c3a3e76f9f6a47a962b21f774cc6001310
ManavShah2400 <shahmanaviant@gmail.com>
2025-09-05 14:24:08 -0400

    Deployment

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ app.get("/", (req, res) => {
 });
 
 // Start server
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
