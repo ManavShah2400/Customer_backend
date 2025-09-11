fb0a6db020c4c57130f5d844b83aa130f5663970
ManavShah2400 <shahmanaviant@gmail.com>
2025-09-11 19:25:38 -0400

    Edit modal

--- Models/Customer.js
+++ Models/Customer.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const customerSchema = new Schema({
     name: { type: String, requried: true },
-    phone_number: { type: String, requried: true },
+    phone_number: { type: String },
     address: { type: String },
     driver_license: { type: String, requried: true },
     company_name: { type: String, requried: true },
